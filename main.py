72f2d96bf668c17050f40d00b8703d488ca86807
JosephSiemering <siemeringjoe@gmail.com>
2025-08-05 18:06:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ st.set_page_config(page_title="Joey's Finances", page_icon="💰", layout="wide"
 category_file = "category_mappings.json"
 
 
-# Function to assign categories to each transcation row
+# Function to assign categories to each transaction
 def categorize_transactions(df, categories):
-    df["Category"] = "Uncategorized"  # Initialize all rows as uncategorized
+    # Initialize all transactions as uncategorized
+    df["Category"] = "Uncategorized"
 
     # Loop through each category and its keywords. The items() method returns all key-value pairs in a dictionary
     for category, keywords in categories.items():
@@ -25,6 +26,7 @@ def categorize_transactions(df, categories):
 
         lowered_keywords = [keyword.lower().strip() for keyword in keywords]
 
+        # If keyword exists in mapping, assign appropriate category
         for idx, row in df.iterrows():  # Loop through each row in DataFrame
             description = row["Description"].lower().strip()
             for keyword in lowered_keywords:
@@ -35,6 +37,7 @@ def categorize_transactions(df, categories):
     return df
 
 
+# Load transactions from file into pandas dataframe, and call categorize_transactions
 def load_transactions(file, categories):
     try:
         df = pd.read_csv(file)
@@ -53,7 +56,7 @@ def load_transactions(file, categories):
 def main():
     st.title("Joey's Finance Dashboard")
 
-    # Then, check if category is already mapped, and update it
+    # Check if json file exists. If it does, open it and load into Python dictionary called 'categories'
     if os.path.exists(category_file):
         with open(category_file, "r") as f:
             categories = json.load(f)
@@ -69,19 +72,55 @@ def main():
         df = load_transactions(uploaded_file, categories)
 
         if df is not None:
+            # =================================================
+            # Set up to get only last 12 months of data
+            # =================================================
             today = datetime.date.today()
             first_day_this_month = today.replace(day=1)
 
+            # Convert date to pandas Timestamp
             first_day_ts = pd.Timestamp(first_day_this_month)
+            # Gives first day 12 months ago
             start_date_ts = first_day_ts - pd.DateOffset(months=11)
-            start_date = start_date_ts.date()
+            start_date = start_date_ts.date()  # convert back to date
 
             end_date = today
 
             df_last12 = df[(df["Date"] >= start_date)
                            & (df["Date"] <= end_date)]
 
+            # =================================================
+            # Graph 1: Cumulative Net Balance
+            # =================================================
+            df_sorted = df_last12.sort_values("Date")
+            # pandas method - creates cumulative sum column
+            df_sorted["Net Balance"] = df_sorted["Amount"].cumsum()
+
+            # Create line chart using plotly express
+            cnb_fig = px.line(df_sorted,
+                              x="Date",
+                              y="Net Balance",
+                              title="Cumulative Net Balance Over Time")
+
+            # Format x axis
+            cnb_fig.update_xaxes(
+                dtick="M1",  # Tick at every month
+                tickformat="%b %Y",
+                ticklabelmode="period"  # Center labels in middle of month
+            )
+
+            # Add horizontal line at x-axis
+            cnb_fig.add_hline(y=0, line_dash="solid", line_color="black")
+
+            # Update y axis label
+            cnb_fig.update_layout(yaxis_title="Balance ($)")
+
+            # embed plotly chart in streamlit
+            st.plotly_chart(cnb_fig)
+
+            # =================================================
             # Table 1: Credits and Debits tabs
+            # =================================================
             debits_df = df_last12[df_last12["Amount"] < 0].copy()
             credits_df = df_last12[df_last12["Amount"] >= 0].copy()
 
